6ad25aadbcf4f5060fbaa976d68cc763c06279fe
wjhowell <wjhowell@umich.edu>
2016-03-25 16:49:18 -0400

    initial commit

---
+++ Assets/foot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class foot : MonoBehaviour {
+	public float kickPower;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Ball" && playerMovement.S.kickbool) {
+			coll.rigidbody.AddForce (Vector2.up * kickPower, ForceMode2D.Impulse);
+		}
+	}
+
+
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+	public static playerMovement S;
+
+	Rigidbody2D rigid;
+	Animator anim;
+	BoxCollider2D foot;
+
+	public float speed = 0f;
+	public float jump = 0f;
+	public float moveX = 0f;
+	public float moveY = 0f;
+	public float kick = 0f;
+	public bool kickbool;
+
+	public int goalsScored;
+
+	void Start () {
+		S = this;
+		rigid = GetComponent<Rigidbody2D> ();
+		anim = GetComponentInChildren<Animator> ();
+		foot = GetComponentInChildren<BoxCollider2D> ();
+
+		goalsScored = 0;
+	}
+	
+	void FixedUpdate () {
+		moveX = Input.GetAxis ("Horizontal");
+		moveY = Input.GetAxis ("Vertical");
+		kick = Input.GetAxis ("Jump");//spacebar
+		rigid.velocity = new Vector2 (moveX * speed, rigid.velocity.y);
+		kickbool = System.Convert.ToBoolean (kick);
+		anim.SetBool ("kick", kickbool);
+	}
+
+	void OnCollisionStay2D(Collision2D coll )
+	{
+		if (coll.gameObject.tag == "Ground" && (Input.GetKey (KeyCode.W))) {//if grounded
+			rigid.AddForce (Vector2.up * jump, ForceMode2D.Impulse);
+		}
+	}
+}
